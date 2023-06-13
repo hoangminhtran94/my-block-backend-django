0181a6a81014312c9f9ee11761597beedca0408d
minhhoangtran94 <95241885+hoangminhtran94@users.noreply.github.com>
2023-06-13 14:41:48 -0400

    update environment and requirements

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = environ.get("SECRET_KEY")
+SECRET_KEY = environ.get(
+    "SECRET_KEY", "django-insecure-57b+#)%422y6@r^h=@^%-e%%h!!9x(e#d7&88=2d(@#u3j=-kw")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = environ.get("IS_DEVELOPMENT", True)
